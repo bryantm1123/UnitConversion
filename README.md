2d3ad6005d2063bbf08193042fa2d54f62302308
Matt <bryantm1122@gmail.com>
2023-05-08 19:34:17 +0200

    added converter and tests for 12 to 24 hour and vice versa

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # UnitConversion
 
-This package is a simple wrapper around some of the conversions provided by the native [Measurement API](https://developer.apple.com/documentation/foundation/measurement). So far it covers conversions for temperature, distance, and time.
+This package is a simple wrapper around some of the conversions provided by the native [Measurement API](https://developer.apple.com/documentation/foundation/measurement). So far it covers conversions for temperature, distance, and time. Also contains a custom converter for converting between 12 hour and 24 hour clock time.
--- Sources/UnitConversion/UnitConversionProvider.swift
+++ Sources/UnitConversion/UnitConversionProvider.swift
@@ -4,6 +4,7 @@ public protocol UnitConversionProviding {
     func convertTemperature(value: Double, fromUnit: UnitTemperature, toUnit: UnitTemperature) -> Measurement<UnitTemperature>
     func convertDistance(value: Double, fromUnit: UnitLength, toUnit: UnitLength) -> Measurement<UnitLength>
     func convertTime(value: Double, fromUnit: UnitDuration, toUnit: UnitDuration) -> Measurement<UnitDuration>
+    func convertTime(from time: String) -> String?
 }
 
 final class UnitConversionProvider: UnitConversionProviding {
@@ -17,5 +18,25 @@ final class UnitConversionProvider: UnitConversionProviding {
     
     func convertTime(value: Double, fromUnit: UnitDuration, toUnit: UnitDuration) -> Measurement<UnitDuration> {
         Measurement(value: value, unit: fromUnit).converted(to: toUnit)
+    }
+    
+    func convertTime(from time: String) -> String? {
+        let dateFormatter = DateFormatter()
+        
+        // Handle 12 hour to 24 hour conversion
+        dateFormatter.dateFormat = "h:mm a"
+        if let date = dateFormatter.date(from: time) {
+            dateFormatter.dateFormat = "HH:mm"
+            return dateFormatter.string(from: date)
+        }
+        
+        // Handle 24 hour to 12 hour conversion
+        dateFormatter.dateFormat = "HH:mm"
+        if let date = dateFormatter.date(from: time) {
+            dateFormatter.dateFormat = "h:mm a"
+            return dateFormatter.string(from: date)
+        }
+
+        return nil // Invalid input format
     }
 }
